9f7fc9437b42214b93e58d0117c9a766862cc6dc
wahidherlambang <43497169+wahidherlambang@users.noreply.github.com>
2018-09-22 23:01:50 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117064/latihan2/deklarasivariabel/PBO210117064Latihan2DeklarasiVariabel.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117064.latihan2.deklarasivariabel;
+
+/**
+ *
+ * @author WAHID HERLAMBANG
+ * NAMA     : Wahid Herlambang Suroso
+ * KELAS    : PBO2
+ * NIM      : 10117064
+ * Deskripsi Program : Program ini berisi program untuk menampilkan 
+ * Deklarasi Variabel
+ */
+public class PBO210117064Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = " + nilaiKarakter);
+    }
+    
+}
